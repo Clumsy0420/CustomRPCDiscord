e432bb66e61906dc21d2cb2efa5f56b1d8db9165
Clumsy0420 <93628528+Clumsy0420@users.noreply.github.com>
2021-11-25 23:09:00 -0500

    Added support and presets

--- main.py
+++ main.py
@@ -3,53 +3,67 @@ import ctypes
 import os
 from pypresence import Presence
 
-def cls():
-    os.system('cls')
+#Declaring Variables
+small_img_key = None
+url = None
+button_name = None
+ClientID = None
+details = None
+status = None
+game = None
 
-#Declaring Variables incase user doesn't want images or buttons
-large_img_key = "Breaks code if not included"
-small_img_key = "Breaks code if not included"
-url = "URL for button"
-button_name = "Name of the button"
-use_buttons = "if user wants buttons"
-how_many_buttons = "How many buttons user wants"
-url2 = "URL for button"
-button_name2 = "Name of the button"
-
-ctypes.windll.kernel32.SetConsoleTitleW("Discord RPC by Clumsy#0420")
-
-use_large_imgs = input("Would you like to use large images Y/N? ").lower()
-if use_large_imgs == "y":
-    large_img_key = input("What's your image key? ").lower()
-
-use_small_imgs = input("Would you like to use Small images Y/N? ").lower()
-if use_small_imgs == "y":
-    small_img_key = input("What's your image key? ")
-
-use_buttons = input("Would you like to use buttons? Y/N? ").lower()
-# how_many_buttons = input("How many buttons would you like? (limit = 2): ")
-button_name = input("What would you like the button's name to be? ")
-url = input("What website would you like to link? (MAKE SURE TO INCLUDE ENTIRE URL): ")
-# if how_many_buttons == "2":
-#     button_name2 = input("What would you like the second button's name to be? ")
-#     url2 = input("What website would you like to link? (MAKE SURE TO INCLUDE ENTIRE URL): ")
-
-What_Status = input("What Would you like your Status to say? ")
-What_Description = input("What Would you like your description to say? ")
-ClientID = input("Please input your Client ID: ").lower()
-
-cls()
-
-#Connecting to ClientID for RPC
-
+print("Black ops 3 = bo3")
+print("Phasmopobia = phasmo")
+print("Minecraft Java = java")
+print("Minecraft Bedrock = bedrock")
+print("Custom RPC = custom")
+game = input("Which game RPC would you like: ").lower()
+if game == "bo3":
+    large_img_key = "bo3"
+    small_img_key = "logo"
+    ClientID = "909565656549171272"
+    button_name = "Get Black Ops 3"
+    url = "https://store.steampowered.com/app/311210/Call_of_Duty_Black_Ops_III/"
+    status = "Wanna kill em together?"
+    details = "Curently Killin Zombies"
+elif game == "phasmo":
+    large_img_key = "logo"
+    small_img_key = "steam"
+    url = "https://store.steampowered.com/app/739630/Phasmophobia/"
+    button_name = "Get Phasmopobia"
+    ClientID = "913238116242948186"
+    details = "Currently Ghost Huntin"
+    status = "Why Not Join em!"
+elif game == "java":
+    large_img_key = "java"
+    small_img_key = "logo"
+    url = "https://www.minecraft.net/en-us"
+    button_name = "Get Minecraft Java"
+    ClientID = "909912354622427186"
+    details = "Slayin the Ender Dragon"
+    status = "Wanna Join em?"
+elif game == "bedrock":
+    large_img_key = "game"
+    small_img_key = "logo"
+    url = "https://www.xbox.com/en-US/games/store/minecraft-for-windows/9nblggh2jhxj"
+    button_name = "Get Minecraft Bedrock"
+    ClientID = "837920143195111475"
+    details = "Feeding the Bees"
+    status = "Why not kill em?"
+elif game == "custom":
+    large_img_key = input("What's your large image key? ")
+    small_img_key = input("What's your small image key? ")
+    url = input("What would you like your buttons url to be? ")
+    button_name = input ("What would you like your buttons name to be? ")
+    details = input("What's your details? ")
+    status = input("What's your status? ")
+    ClientID = input("What's your client ID: ")
+# Connecting to ClientID for RPC
 RPC = Presence(ClientID)
 RPC.connect()
 print("Succesfully Connected to RPC")
 
-
-
-RPC.update(large_image=large_img_key, small_image=small_img_key, buttons=[{"label": button_name, "url": url}], state=What_Status, details=What_Description)
+RPC.update(large_image=large_img_key, small_image=small_img_key,details=details, state=status, buttons=[{"label": button_name, "url": url}])
 
 while True:
-    time.sleep(15) 
-
+    time.sleep(15)
